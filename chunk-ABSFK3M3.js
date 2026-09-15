89f1f95570c59f88769007438d5fa01438cd1791
Jfl0r3s <flores.acostajayrojimmy@gmail.com>
2026-09-14 23:58:49 -0400

    Auto-generated commit

--- chunk-ABSFK3M3.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YEZFQY7F.js";import"./chunk-YWPGQDZP.js";export{a as About};
---
+++ chunk-ALLHDILJ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-JALIYGMB.js";import"./chunk-YWPGQDZP.js";export{a as About};
